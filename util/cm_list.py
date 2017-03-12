37cc9530c6dfc11f153e3f38d54b2b4c339eeccf
xyy_xde <bewantbe@gmail.com>
2017-03-12 20:50:59 +0400

    Remove unused and ugly colormaps.
    Some "perceptual unifrom" colormaps is not added(see util/*.csv), since the improvement is small, I don't want to confuse user.

--- util/cm_list.py
+++ util/cm_list.py
@@ -9,7 +9,7 @@
 import csv
 import numpy as np
 #import matplotlib.pyplot as plt  # matplotlib ver >= 1.5
-names = ['magma', 'inferno', 'plasma', 'viridis']
+names = ['magma', 'inferno', 'plasma', 'viridis', 'blackbody_uniform']
 
 for c_name in names:
 #    cmap = np.array(plt.get_cmap(c_name).colors)  # if use matplotlib
@@ -17,7 +17,10 @@ for c_name in names:
         reader = csv.reader(f)
         cmap = np.array(list(reader)).astype(np.float)
     #Note: len(cmap) == 256
-    cm = np.floor(cmap[::-1] * 255.99);
+    if len(cmap)==256:
+        cm = np.floor(cmap[::-1] * 255.99);
+    else:
+        cm = np.vstack((np.floor(cmap[::-1] * 255.99),[0,0,0]))
     cm[-1] = [0, 0, 0]      # make last block black
     s = ", ".join([("0x%06x" % (c[0] * 2**16 + c[1] * 2**8 + c[2])) for c in cm])
     s2 = '\n'.join([s[0+i:80+i] for i in range(0, len(s), 80)])
